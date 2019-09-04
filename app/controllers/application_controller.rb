dbcde54fc4d9956ce0c40728d64bc2b2444740f0
Kelly Sandeson <kellysandeson@gmail.com>
2019-09-04 16:02:51 -0600

    Create methods for different current users
    Co-authored-by:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart
+  helper_method :cart, :current_user, :current_employee, :current_merchant, :current_admin
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_employee
+    current_user && current_user.employee?
+  end
+
+  def current_merchant
+    current_user && current_user.merchant?
+  end
+
+  def current_admin
+    current_user && current_user.admin?
   end
 
 end
