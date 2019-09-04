c1bbe3c097cd81f1481c48271bc6355d4a75922e
Nancy Lee <srlee713@gmail.com>
2019-09-04 16:41:15 -0600

    Add attributes validation in user model
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
                    Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
                    Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :address, :city, :state, :zipcode, :password_digest, :password_confirmation
-  validates :email, uniqueness: true, presence: true
+  validates_presence_of :name, :address, :city, :state, :zipcode, :password_digest
+  validates :email,
+            uniqueness: true,
+            presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
 end
