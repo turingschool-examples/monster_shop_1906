b3830e309e9bee69ea6861a10ef9102156b86759
Corina Allen <StarPerfect@gmail.com>
2019-09-05 15:58:23 -0600

    Create Login Spec for Users
    Co-authored-by: Scott Payton <133855435+wolfeJob@users.noreply.github.com>
    Co-authored-by: Kelly Sandeson <46202366+ksandy95@users.noreply.github.com>
    Co-authored-by: Santiago Caraveo <43061381+Santiag0C@users.noreply.github.com>

--- spec/features/users/login_logout_spec.rb
+++ spec/features/users/login_logout_spec.rb
@@ -11,7 +11,7 @@ require 'rails_helper'
 RSpec.describe 'Login/Logout Functionality' do
   describe 'when a user visits the login path' do
     before :each do
-      @corina = User.create(name: 'Corina Allen', address: '1488 S. Kenton', city: 'Aurora', state: 'CO', zip:, email: 'StarPerfect@gmail.com', password: 'Hello123', role: 2)
+      @corina = User.create(name: 'Corina Allen', address: '1488 S. Kenton', city: 'Aurora', state: 'CO', zip: '80014', email: 'StarPerfect@gmail.com', password: 'Hello123', role: 2)
       @scott = User.create(name: "Scott Payton", address: "123 Scott Street", city: "Stapleton", state: "CO", zip: "80011", email:"bigfun@gmail.com", password: "mine", role: 0 )
       @billy = User.create(name: "Billy Joel", address: "123 Billy Street", city: "Denver", state: "CO", zip: "80011", email:"billobill@gmail.com", password: "yours", role: 1 )
       @kate = User.create(name: "Kate Long", address: "123 Kate Street", city: "Fort Collins", state: "CO", zip: "80011", email:"kateaswesome@gmail.com", password: "ours", role: 3)
@@ -23,9 +23,10 @@ RSpec.describe 'Login/Logout Functionality' do
       fill_in :email, with: 'bigfun@gmail.com'
       fill_in :password, with: 'mine'
 
-      click_on 'Login'
+      click_button 'Login'
 
-      expect(current_path).to eq(profile_path)
+      expect(current_path).to eq('/')
+      expect(page).to have_link('Profile')
       expect(page).to have_content("Logged in as #{@scott.name}")
     end
 
@@ -34,7 +35,7 @@ RSpec.describe 'Login/Logout Functionality' do
       fill_in :email, with: 'billobill@gmail.com'
       fill_in :password, with: 'yours'
 
-      click_on 'Login'
+      click_button 'Login'
 
       expect(page).to have_link('Merchant Dashboard')
     end
@@ -44,7 +45,7 @@ RSpec.describe 'Login/Logout Functionality' do
       fill_in :email, with: 'StarPerfect@gmail.com'
       fill_in :password, with: 'Hello123'
 
-      click_on 'Login'
+      click_button 'Login'
 
       expect(page).to have_link('Merchant Dashboard')
     end
@@ -54,7 +55,7 @@ RSpec.describe 'Login/Logout Functionality' do
       fill_in :email, with: 'kateaswesome@gmail.com'
       fill_in :password, with: 'ours'
 
-      click_on 'Login'
+      click_button 'Login'
 
       expect(page).to have_link('Admin Dashboard')
       expect(page).to have_link('Users')
