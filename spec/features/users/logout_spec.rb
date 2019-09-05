c9070d0fe0a135fec950866543b1a4ec1e8f36ec
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-05 14:55:00 -0600

    Adds logging out test + cart amount changing to tests
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- spec/features/users/logout_spec.rb
+++ spec/features/users/logout_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe "User Logout" do
                   zipcode: "77652",
                   email: "junglegeorge@email.com",
                   password: "Tree123")
+
+    @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+    @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
   end
 
   it "can logout user and redirect to the home page with a flash messages indicating that I am logged out and all shopping cart items are deleted" do
@@ -20,12 +24,15 @@ RSpec.describe "User Logout" do
 
     click_button "Submit"
 
+    visit "/items/#{@paper.id}"
+      click_on "Add To Cart"
+
     within 'nav' do
       click_link 'Logout'
     end
 
     expect(current_path).to eq(root_path)
-    expect(page).to have_content("You are logged out!")
+    expect(page).to have_content("You have been logged out!")
     expect(page).to have_content("Cart: 0")
   end
 end
