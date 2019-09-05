cc7a3483beb360b6847244e0f52a5aaf2684c99a
Corina Allen <StarPerfect@gmail.com>
2019-09-05 15:57:43 -0600

    Update Seeds
    Co-authored-by: Scott Payton <133855435+wolfeJob@users.noreply.github.com>
    Co-authored-by: Kelly Sandeson <46202366+ksandy95@users.noreply.github.com>
    Co-authored-by: Santiago Caraveo <43061381+Santiag0C@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
 
 #users
-corina = User.create(name: 'Corina Allen', address: '1488 S. Kenton', city: 'Aurora', state: 'CO', zip:, email: 'StarPerfect@gmail.com', password: 'Hello123', role: 2)
+corina = User.create(name: 'Corina Allen', address: '1488 S. Kenton', city: 'Aurora', state: 'CO', zip:'80014', email: 'StarPerfect@gmail.com', password: 'Hello123', role: 2)
 scott = User.create(name: "Scott Payton", address: "123 Scott Street", city: "Stapleton", state: "CO", zip: "80011", email:"bigfun@gmail.com", password: "mine", role: 0 )
 billy = User.create(name: "Billy Joel", address: "123 Billy Street", city: "Denver", state: "CO", zip: "80011", email:"billobill@gmail.com", password: "yours", role: 1 )
 kate = User.create(name: "Kate Long", address: "123 Kate Street", city: "Fort Collins", state: "CO", zip: "80011", email:"kateaswesome@gmail.com", password: "ours", role:2 )
