94f8f35e4baa2916d4c95431f00918829ebd801a
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-05 00:05:20 -0600

    spacing changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  
   validates_presence_of :name, :address, :city, :state, :zipcode, :password_digest
   validates :email,
             uniqueness: true,
