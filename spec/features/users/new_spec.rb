1e1a8719f4bedfdda7c835746269f871089dc80d
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-05 08:39:18 -0600

    Updates user new_spec to include flash error message
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -30,25 +30,14 @@ RSpec.describe "Registering User" do
     expect(page).to_not have_link("Login")
     expect(page).to_not have_link("Register")
   end
+
+  it "when I visit the user registration page and do not fill in the form completely, I am returned to the registration page and see a flash message that I am missing required fields" do
+
+    visit "/register"
+
+    click_on "Submit"
+
+    expect(current_path).to eq("/register")
+    expect(page).to have_content("Name can't be blank, Address can't be blank, City can't be blank, State can't be blank, Zipcode can't be blank, Password digest can't be blank, Email can't be blank, Email is invalid, and Password can't be blank")
+  end
 end
-
-
-# As a visitor
-# When I click on the 'register' link in the nav bar
-# Then I am on the user registration page ('/register')
-# And I see a form where I input the following data:
-# - my name
-# - my street address
-# - my city
-# - my state
-# - my zip code
-# - my email address
-# - my preferred password
-# - a confirmation field for my password
-#
-# When I fill in this form completely,
-# And with a unique email address not already in the system
-# My details are saved in the database
-# Then I am logged in as a registered user
-# I am taken to my profile page ("/profile")
-# I see a flash message indicating that I am now registered and logged in
