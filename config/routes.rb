66a0af7058823193e3e4a56cb8c22d8c1e79a4fa
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-06 15:48:24 -0600

    Deleted route for welcome signin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "welcome#index"
-  get "/signin", to: "welcome#signin"
 
   resources :merchants do
     resources :items, only: [:index, :new, :create]
