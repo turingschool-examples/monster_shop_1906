20054524a1a98c55833e75afb599aedff839f0e6
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-06 08:03:40 -0600

    Adds require merchant employee to base controller

--- app/controllers/merchant/base_controller.rb
+++ app/controllers/merchant/base_controller.rb
@@ -1,7 +1,12 @@
 class Merchant::BaseController < ApplicationController
+  before_action :require_merchant_employee
   before_action :require_merchant_admin
 
   def require_merchant_admin
     render file: "/public/404" unless current_merchant_admin?
+  end
+
+  def require_merchant_employee
+    render file: "/public/404" unless current_merchant_employee?
   end
 end
