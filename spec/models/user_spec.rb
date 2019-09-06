7efb9a8142d4c72c09fda3e561dff939d36c8f2b
Nancy Lee <srlee713@gmail.com>
2019-09-06 09:03:21 -0600

    Add user role test in user model
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
                    Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
                    Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,47 @@ describe User, type: :model do
     it {should allow_value('user@example.com').for(:email)}
     it {should_not allow_value("foo").for(:email)}
   end
+
+  describe "roles" do
+    it "can be created as a default user" do
+      regular_user = User.create!(name: "George Jungle",
+                    address: "1 Jungle Way",
+                    city: "Jungleopolis",
+                    state: "FL",
+                    zipcode: "77652",
+                    email: "junglegeorge@email.com",
+                    password: "Tree123")
+
+      expect(regular_user.role).to eq("regular_user")
+      expect(regular_user.default?).to be_truthy
+    end
+
+    it "can be created as a merchant user" do
+      merchant_user = User.create!(name: "Michael Scott",
+                    address: "1725 Slough Ave",
+                    city: "Scranton",
+                    state: "PA",
+                    zipcode: "18501",
+                    email: "michael.s@email.com",
+                    password: "WorldBestBoss",
+                    role: 1)
+
+      expect(merchant_user.role).to eq("merchant_user")
+      expect(merchant_user.merchant_user?).to be_truthy
+    end
+
+    it "can be created as a admin user" do
+      admin_user = User.create!(name: "Leslie Knope",
+                    address: "14 Somewhere Ave",
+                    city: "Pawnee",
+                    state: "IN",
+                    zipcode: "18501",
+                    email: "recoffice@email.com",
+                    password: "Waffles",
+                    role: 3)
+
+      expect(admin_user.role).to eq("admin_user")
+      expect(admin_user.admin_user?).to be_truthy
+    end
+  end
 end
