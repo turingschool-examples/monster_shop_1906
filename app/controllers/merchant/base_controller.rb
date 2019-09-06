e5d430893ccd6b4aee558fa349b19c0ec12b5464
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-05 20:44:45 -0600

    Minor spacing issue

--- app/controllers/merchant/base_controller.rb
+++ app/controllers/merchant/base_controller.rb
@@ -2,6 +2,7 @@ class Merchant::BaseController < ApplicationController
   before_action :require_merchant_admin
 
   def require_merchant_admin
+
     render file: "/public/404" unless current_merchant_admin?
   end
 end
