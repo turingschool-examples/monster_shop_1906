4f61cc24d58e1a1534b5620a628f3753cfffb09a
Scott Payton <spayton.c@gmail.com>
2019-09-06 14:43:17 -0600

    Created show action in Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
 
   def create
@@ -15,11 +17,12 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.last
   end
 
   private
 
   def user_params
-    params.permit(:name, :address, :city, :state, :zip, :email, :password)
+    params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
   end
 end
