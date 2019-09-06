b06ace9d59a3789687c0f08335117d4fa51840cc
Kelly Sandeson <kellysandeson@gmail.com>
2019-09-06 15:01:06 -0600

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -38,5 +38,10 @@ Rails.application.routes.draw do
 
   get '/register', to: 'users#new'
   post '/users', to: 'users#create'
+
   get '/profile', to: 'users#show', as: :profile
+
+  get '/dashboard', to: 'admin/dashboard#show', as: :admin_dashboard
+
+  get '/dashboard', to: 'merchant/dashboard#show', as: :merchant_dashboard
 end
