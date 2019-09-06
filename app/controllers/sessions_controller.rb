094b074e1d5bc7b89c51097527e08abaaabb05cd
Corina Allen <StarPerfect@gmail.com>
2019-09-06 15:12:41 -0600

    Update SessionsController with Private Login_Redirect method and add authentication to Create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,32 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    session[:user_id] = user.id
-    flash[:success] = "Welcome, #{user.name}!"
-    redirect_to '/'
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
+      login_redirection
+    else
+      flash[:error] = 'Invalid Credentials, please try again.'
+      render :new
+    end
+  end
+
+
+
+
+
+
+  private
+  def login_redirection
+    user = User.find_by(email: params[:email])
+    if user.default?
+      redirect_to profile_path
+    elsif user.employee?
+      redirect_to profile_path
+    elsif user.merchant?
+      redirect_to merchant_dashboard_path
+    else user.admin?
+      redirect_to admin_dashboard_path
+    end
   end
 end
