08ba590d1e94d95efc419f9687171bc5f559ca9a
Nancy Lee <srlee713@gmail.com>
2019-09-07 10:50:42 -0600

    Add status method in item order model
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
                    Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
                    Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,6 +1,6 @@
 class ItemOrder <ApplicationRecord
-  validates_presence_of :item_id, :order_id, :price, :quantity
-
+  validates_presence_of :item_id, :order_id, :price, :quantity, :status
+  validates :status, inclusion: {:in => ['pending', 'packaged', 'shipped', 'cancelled']}
   belongs_to :item
   belongs_to :order
   belongs_to :user
