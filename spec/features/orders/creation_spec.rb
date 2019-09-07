51716d59f50af8da365eb118de669e9074c70474
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-06 18:16:53 -0600

    Updates syntax/deletes commented out tests

--- spec/features/orders/creation_spec.rb
+++ spec/features/orders/creation_spec.rb
@@ -1,11 +1,3 @@
-# When I fill out all information on the new order page
-# And click on 'Create Order'
-# An order is created and saved in the database
-# And I am redirected to that order's show page with the following information:
-#
-# - Details of the order:
-
-# - the date when the order was created
 RSpec.describe("Order Creation") do
   describe "When I check out from my cart" do
     before(:each) do
@@ -95,7 +87,7 @@ RSpec.describe("Order Creation") do
       end
 
       within "#datecreated" do
-        expect(page).to have_content(new_order.created_at)
+        expect(page).to have_content(new_order.created_at.strftime('%D'))
       end
     end
 
