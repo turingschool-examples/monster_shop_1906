516dbbe5d9c8d5a38115d9a2bc03bce99e0de7c9
Corina Allen <StarPerfect@gmail.com>
2019-09-07 11:41:44 -0600

    Update create action with if user && user.find_by

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
       login_redirection
