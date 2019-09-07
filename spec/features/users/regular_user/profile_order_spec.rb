11bd8beb07ffb977e4ce656c1964466a9f6873c7
Nancy Lee <srlee713@gmail.com>
2019-09-07 14:04:45 -0600

    Modify test cases in profile order spec test
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
                    Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
                    Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- spec/features/users/regular_user/profile_order_spec.rb
+++ spec/features/users/regular_user/profile_order_spec.rb
@@ -39,15 +39,31 @@ RSpec.describe "User Profile Order Page" do
     end
 
     within "#item-order-#{@item_order_1.id}" do
-      expect(@item_order_1.status).to eq("pending")
       expect(page).to have_content(@item_1.name)
+      expect(page).to have_content(@item_1.merchant.name)
+      expect(page).to have_content(@item_1.price)
+      expect(page).to have_content(@item_order_1.quantity)
+      expect(page).to have_content(@item_order_1.subtotal)
       expect(page).to have_content(@order_1.name)
+      expect(page).to have_content(@order_1.address)
+      expect(page).to have_content(@order_1.city)
+      expect(page).to have_content(@order_1.state)
+      expect(page).to have_content(@order_1.zip)
+      expect(page).to have_content(@item_order_1.status)
     end
 
     within "#item-order-#{@item_order_2.id}" do
-      expect(@item_order_2.status).to eq("pending")
       expect(page).to have_content(@item_2.name)
+      expect(page).to have_content(@item_2.merchant.name)
+      expect(page).to have_content(@item_2.price)
+      expect(page).to have_content(@item_order_2.quantity)
+      expect(page).to have_content(@item_order_2.subtotal)
       expect(page).to have_content(@order_1.name)
+      expect(page).to have_content(@order_1.address)
+      expect(page).to have_content(@order_1.city)
+      expect(page).to have_content(@order_1.state)
+      expect(page).to have_content(@order_1.zip)
+      expect(page).to have_content(@item_order_2.status)
     end
   end
 end
