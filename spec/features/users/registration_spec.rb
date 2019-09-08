a2382726845c7dd7969ead1f0a1dcba1c4aba170
Corina Allen <StarPerfect@gmail.com>
2019-09-08 17:05:25 -0600

    Remove commented out code

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -1,22 +1,3 @@
-# As a visitor
-# When I click on the 'register' link in the nav bar
-# Then I am on the user registration page ('/register')
-# And I see a form where I input the following data:
-# - my name
-# - my street address
-# - my city
-# - my state
-# - my zip code
-# - my email address
-# - my preferred password
-# - a password_confirmationation field for my password
-#
-# When I fill in this form completely,
-# And with a unique email address not already in the system
-# My details are saved in the database
-# Then I am logged in as a registered user
-# I am taken to my profile page ("/profile")
-# I see a flash message indicating that I am now registered and logged in
 require 'rails_helper'
 
 RSpec.describe 'As a visitor I see a link to register on the nav bar' do
@@ -40,11 +21,7 @@ RSpec.describe 'As a visitor I see a link to register on the nav bar' do
     expect(page).to have_content('You are now a registered user and have been logged in.')
   end
 end
-# As a visitor
-# When I visit the user registration page
-# And I do not fill in this form completely,
-# I am returned to the registration page
-# And I see a flash message indicating that I am missing required fields
+
 RSpec.describe 'Incomplete registration form' do
   it 'sees a flash notification' do
     visit '/register'
@@ -65,20 +42,13 @@ RSpec.describe 'Incomplete registration form' do
     expect(page).to have_content("Password can't be blank")
   end
 end
-# As a visitor
-# When I visit the user registration page
-# If I fill out the registration form
-# But include an email address already in the system
-# Then I am returned to the registration page
-# My details are not saved and I am not logged in
-# The form is filled in with all previous data except the email field and password fields
-# I see a flash message telling me the email address is already in use
+
 RSpec.describe 'Not Unique Email for registration' do
   it 'sees flash notification' do
     billy = User.create(name: "Billy Joel", address: "123 Billy Street", city: "Denver", state: "CO", zip: "80011", email:"billobill@gmail.com", password: "mine" )
     kate = User.create(name: "Kate Long", address: "123 Kate Street", city: "Fort Collins", state: "CO", zip: "80011", email:"kateaswesome@gmail.com", password: "mine4" )
     user = User.create(name:"Santiago", address:"123 tree st", city:"lakewood", state:"CO", zip: "19283", email:"santamonica@hotmail.com", role:3, password: "mine3 ")
-    # binding.pry
+
     visit "/register"
     fill_in :name, with: 'Scotty'
     fill_in :address, with: '1488 S Kenton St'
