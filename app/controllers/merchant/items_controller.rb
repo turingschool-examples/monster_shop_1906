10d6a2b5203100c0684b89c6ad07d91060f6bdfc
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-08 12:36:31 -0600

    Removed dead code, spacing format

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,10 +1,5 @@
 class Merchant::ItemsController < Merchant::BaseController
   def index
-    # binding.pry
     @items = current_user.merchant.items
   end
-  #
-  # def show
-  #   @user = User.find(params[:id])
-  # end
 end
