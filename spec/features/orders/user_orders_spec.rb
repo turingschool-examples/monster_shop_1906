4ac2c989a5c18f1999bb3958d084d2592f3e42c8
Corina Allen <StarPerfect@gmail.com>
2019-09-08 09:15:35 -0600

    Add User Story 27 tests

--- spec/features/orders/user_orders_spec.rb
+++ spec/features/orders/user_orders_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe 'Users Order Show Page' do
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
       @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-      @order_1 = Order.create!(name: @user.name, address: @user.address, city: @user.city, state: @user.state, zip: @user.zip, user_id: @user.id)
-      @order_2 = Order.create!(name: @user.name, address: @user.address, city: @user.city, state: @user.state, zip: @user.zip, user_id: @user.id)
-      @order_3 = Order.create!(name: @user.name, address: @user.address, city: @user.city, state: @user.state, zip: @user.zip, user_id: @user.id)
-      @item_order_1 = @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
-      @item_order_2 = @order_2.item_orders.create!(item: @paper, price: @paper.price, quantity: 20)
-      @item_order_3 = @order_3.item_orders.create!(item: @pencil, price: @pencil.price, quantity: 5)
+      @order_1 = Order.create(name: @user.name, address: @user.address, city: @user.city, state: @user.state, zip: @user.zip, user_id: @user.id)
+      @order_2 = Order.create(name: @user.name, address: @user.address, city: @user.city, state: @user.state, zip: @user.zip, user_id: @user.id)
+      @order_3 = Order.create(name: @user.name, address: @user.address, city: @user.city, state: @user.state, zip: @user.zip, user_id: @user.id)
+      @item_order_1 = @order_1.item_orders.create(item: @tire, price: @tire.price, quantity: 2)
+      @item_order_2 = @order_2.item_orders.create(item: @paper, price: @paper.price, quantity: 20)
+      @item_order_3 = @order_3.item_orders.create(item: @pencil, price: @pencil.price, quantity: 5)
     end
     it 'can click My Orders profile page link to see all their orders' do
 
@@ -29,9 +29,9 @@ RSpec.describe 'Users Order Show Page' do
     the grand total of all items for that order" do
     visit '/profile/orders'
 
-    expect(page).to have_link(@order_1.id)
-    expect(page).to have_link(@order_2.id)
-    expect(page).to have_link(@order_3.id)
+    expect(page).to have_link("Order ID ##{@order_1.id}")
+    expect(page).to have_link("Order ID ##{@order_2.id}")
+    expect(page).to have_link("Order ID ##{@order_3.id}")
 
     expect(page).to have_content(@order_1.created_at)
     expect(page).to have_content(@order_2.created_at)
@@ -41,13 +41,50 @@ RSpec.describe 'Users Order Show Page' do
     expect(page).to have_content(@order_2.status)
     expect(page).to have_content(@order_3.status)
 
-    expect(page).to have_content("Total Order Item Count: #{@order_1.item_orders.count}")
-    expect(page).to have_content("Total Order Item Count: #{@order_2.item_orders.count}")
-    expect(page).to have_content("Total Order Item Count: #{@order_3.item_orders.count}")
+    expect(page).to have_content("Number of Items Ordered: #{@order_1.item_orders.count}")
+    expect(page).to have_content("Number of Items Ordered: #{@order_2.item_orders.count}")
+    expect(page).to have_content("Number of Items Ordered: #{@order_3.item_orders.count}")
 
     expect(page).to have_content(@order_1.grandtotal)
     expect(page).to have_content(@order_2.grandtotal)
     expect(page).to have_content(@order_3.grandtotal)
     end
+
+#     As a registered user
+# When I visit my Profile Orders page
+# And I click on a link for order's show page
+# My URL route is now something like "/profile/orders/15"
+# I see all information about the order, including the following information:
+# - the ID of the order
+# - the date the order was made
+# - the date the order was last updated
+# - the current status of the order
+# - each item I ordered, including name, description, thumbnail, quantity, price and subtotal
+# - the total quantity of items in the whole order
+# - the grand total of all items for that order
+  it 'can click on order from Order History page and view orders details' do
+    visit '/profile/orders'
+
+    click_link "Order ID ##{@order_2.id}"
+save_and_open_page
+    expect(current_path).to eq("/orders/#{@order_2.id}")
+    expect(page).to have_content("Order ID ##{@order_2.id} Details")
+    expect(page).to have_content("Order Placed On: #{@order_2.created_at}")
+    expect(page).to have_content("Last Updated On: #{@order_2.updated_at}")
+    expect(page).to have_content("Current Status: #{@order_2.status}")
+
+    within "#ordered-item-#{@item_order_2.id}" do
+      expect(page).to have_content(@item_order_2.name)
+      expect(page).to have_content(@item_order_2.description)
+      expect(page).to have_content(@item_order_2.image)
+      expect(page).to have_content(@item_order_2.quantity)
+      expect(page).to have_content(@item_order_2.price)
+      expect(page).to have_content(@item_order_2.subtotal)
+  end
+
+    expect(page).to have_content("Number of Items Ordered: #{@order_2.item_orders.count}")
+    expect(page).to have_content("Order Total: $#{@order_2.grandtotal}")
+  end
+
   end
 end
