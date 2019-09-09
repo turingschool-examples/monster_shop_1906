15fa4dbc075e4b4619510c3a38411b7ea003f8d6
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-08 21:53:26 -0600

    Add method to fulfill item_order, updates ItemOrder status and Item inventory

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -7,4 +7,14 @@ class Merchant::OrdersController < Merchant::BaseController
       render file: "/public/404"
     end
   end
+
+  def fulfill
+    item_order = ItemOrder.where(order: params[:order_id]).find(params[:item_order_id])
+    item_order.update(status: "packaged")
+
+    item_order.item.decrease_inventory(item_order)
+
+    flash[:success] = "Item order #{item_order.id} has been fulfilled"
+    redirect_to merchant_order_path(item_order.order)
+  end
 end
