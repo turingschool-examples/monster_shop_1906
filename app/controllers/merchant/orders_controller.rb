f22884ce6556e19e0b10ea85a26bea3b925afd2c
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-08 18:37:08 -0600

    Sad path if merchant goes to order that is not related to them

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -2,5 +2,9 @@ class Merchant::OrdersController < Merchant::BaseController
   def show
     @order = Order.find(params[:id])
     @item_orders = current_user.item_orders_by_merchant(@order.id)
+
+    if @item_orders.empty?
+      render file: "/public/404"
+    end
   end
 end
