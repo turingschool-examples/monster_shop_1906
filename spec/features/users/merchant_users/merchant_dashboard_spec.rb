e2ebfa54adfe0dca84a22a4617ddb438625e4453
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-08 18:40:54 -0600

    Marked as dead test, to be deleted

--- spec/features/users/merchant_users/merchant_dashboard_spec.rb
+++ spec/features/users/merchant_users/merchant_dashboard_spec.rb
@@ -1,37 +1,40 @@
-require 'rails_helper'
+#DEAD TEST to be deleted
 
-RSpec.describe "Merchant Dashboard" do
-  before :each do
-    @bike_shop = Merchant.create!(name: "Brian's Bike Shop",
-                  address: '123 Bike Rd.',
-                  city: 'Richmond',
-                  state: 'VA',
-                  zip: 23137)
-
-    @chain = @bike_shop.items.create!(name: "Chain",
-                  description: "It'll never break!",
-                  price: 50,
-                  image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588",
-                  inventory: 5)
-
-    @merchant_admin = @bike_shop.users.create!(name: "Michael Scott",
-                  address: "1725 Slough Ave",
-                  city: "Scranton",
-                  state: "PA",
-                  zipcode: "18501",
-                  email: "michael.s@email.com",
-                  password: "WorldBestBoss",
-                  role: 2)
-    @merchant_employee = @bike_shop.users.create!(name: "Dwight Schrute",
-                  address: "1725 Slough Ave",
-                  city: "Scranton",
-                  state: "PA",
-                  zipcode: "18501",
-                  email: "dwight@email.com",
-                  password: "MichaelIsTheBest",
-                  role: 1)
-
-  end
+# require 'rails_helper'
+#
+#
+# RSpec.describe "Merchant Dashboard" do
+#   before :each do
+#     @bike_shop = Merchant.create!(name: "Brian's Bike Shop",
+#                   address: '123 Bike Rd.',
+#                   city: 'Richmond',
+#                   state: 'VA',
+#                   zip: 23137)
+#
+#     @chain = @bike_shop.items.create!(name: "Chain",
+#                   description: "It'll never break!",
+#                   price: 50,
+#                   image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588",
+#                   inventory: 5)
+#
+#     @merchant_admin = @bike_shop.users.create!(name: "Michael Scott",
+#                   address: "1725 Slough Ave",
+#                   city: "Scranton",
+#                   state: "PA",
+#                   zipcode: "18501",
+#                   email: "michael.s@email.com",
+#                   password: "WorldBestBoss",
+#                   role: 2)
+#     @merchant_employee = @bike_shop.users.create!(name: "Dwight Schrute",
+#                   address: "1725 Slough Ave",
+#                   city: "Scranton",
+#                   state: "PA",
+#                   zipcode: "18501",
+#                   email: "dwight@email.com",
+#                   password: "MichaelIsTheBest",
+#                   role: 1)
+#
+#   end
 
   # xit 'merchant admin sees link to view shop items' do
   #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin)
