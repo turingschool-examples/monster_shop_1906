b0f606f26b33c77f6cfdc52bf46bd498a8927892
Scott Payton <spayton.c@gmail.com>
2019-09-10 17:07:34 -0600

    Created show action for controllers
    Co-Authored-By: Corina Allen <47605558+StarPerfect@users.noreply.github.com>

--- app/controllers/employee/dashboard_controller.rb
+++ app/controllers/employee/dashboard_controller.rb
@@ -1,5 +1,16 @@
 class Employee::DashboardController < ApplicationController
   def show
     @user = current_user
+    if @user.role == 1 || 2
+      @merchant = Merchant.find(@user.merchant_id)
+    end
   end
+
+
+  # private
+  #
+  # def merchant_params
+  #   params.require(:user).permit(:merchant_id)
+  # end
+
 end
--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,8 +1,18 @@
 class Merchant::DashboardController < ApplicationController
-  # require :merchant
+# require :merchant
 
   def show
     @user = current_user
+    if @user.role == 1 || 2
+      @merchant = Merchant.find(@user.merchant_id)
+    end
+  end
+
+
+  private
+
+  def merchant_params
+    params.require(:user).permit(:merchant_id)
   end
 
 end
