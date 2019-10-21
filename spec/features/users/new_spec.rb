d45a9a534a3243bd9b7502ed28ce87e720b99ce8
Brian Bower <brian@brianbower.com>
2019-10-21 15:29:37 -0600

    Add test for incomplete form
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -21,26 +21,31 @@ RSpec.describe 'New User Page' do
       expect(current_path).to eq('/profile')
       expect(page).to have_content("You are now registered and logged in")
     end
+
+    it "gets an error if form not filled in correctly" do
+      visit '/register'
+
+      click_button 'Complete Registration'
+
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Address can't be blank")
+      expect(page).to have_content("City can't be blank")
+      expect(page).to have_content("State can't be blank")
+      expect(page).to have_content("Zip can't be blank")
+      expect(page).to have_content("Email can't be blank")
+      expect(page).to have_content("Password can't be blank")
+
+      fill_in :name, with: "Bob J"
+      fill_in :address, with: "123 Main St."
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: "80211"
+      fill_in :email, with: "bobj@gmail.com"
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "Password"
+      click_button 'Complete Registration'
+
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
   end
 end
-
-
-
-# When I click on the 'register' link in the nav bar
-# Then I am on the user registration page ('/register')
-# And I see a form where I input the following data:
-# - my name
-# - my street address
-# - my city
-# - my state
-# - my zip code
-# - my email address
-# - my preferred password
-# - a confirmation field for my password
-#
-# When I fill in this form completely,
-# And with a unique email address not already in the system
-# My details are saved in the database
-# Then I am logged in as a registered user
-# I am taken to my profile page ("/profile")
-# I see a flash message indicating that I am now registered and logged in
