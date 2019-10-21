9dbc856a4060693e53ae5959ab2949559add1b3f
Alice Post <alicep2322@gmail.com>
2019-10-21 15:00:46 -0600

    New user registration form
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,18 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find(session[:user_id])
   end
+
+  def create
+    new_user = User.create(user_params)
+    session[:user_id] = new_user.id
+    flash[:success] = "You are now registered and logged in."
+    redirect_to '/profile'
+  end
+
+  private
+    def user_params
+      params.permit(:name,:address,:city,:state,:zip,:email,:password,:password_confirmation)
+    end
 end
