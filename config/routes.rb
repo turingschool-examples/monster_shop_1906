bc75adcf9f48d9fe3ef8f4a74a5ca8731a81bab9
Alice Post <alicep2322@gmail.com>
2019-10-21 15:00:29 -0600

    Login now routes to sessions controller
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/register', to: 'users#new'
-  get '/login', to: 'users#show'
+  post '/users', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  get '/profile', to: 'users#show'
 
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
