f6cee0ff24bbd0968dab21c866ec7701195e092a
Brian Bower <brian@brianbower.com>
2019-10-21 15:45:28 -0600

    Add test for unique email
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -47,5 +47,24 @@ RSpec.describe 'New User Page' do
 
       expect(page).to have_content("Password confirmation doesn't match Password")
     end
+
+    it "cannot create a user with a already used email" do
+
+      user = User.create(name: 'Bob J', address: '123 Fake St', city: 'Denver', state: 'Colorado', zip: 80111, email: 'user@user.com', password: 'password')
+
+      visit '/register'
+
+      fill_in :name, with: "Bob J"
+      fill_in :address, with: "123 Main St."
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: "80211"
+      fill_in :email, with: "user@user.com"
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "Password"
+      click_button 'Complete Registration'
+
+      expect(page).to have_content('Email has already been taken')
+    end
   end
 end
