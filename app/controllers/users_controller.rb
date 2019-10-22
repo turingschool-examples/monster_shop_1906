e2ec80bb6687657be4b665a58c9073823870ef00
Rachel Lew <rlew07@gmail.com>
2019-10-22 15:43:20 -0600

    Adds conditional to create method, permits password_confirmation in user params
    Co-authored-by: Jonathan Patterson <47950816+Jonpatt92@users.noreply.github.com>
    Co-authored-by: Mary Lang <47950816+Jonpatt92@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    new_user = User.create!(user_params)
-    session[:user_id] = new_user.id
-
-    flash[:sucess] = "You have registered successfully! You are now logged in as #{new_user.name}."
-    redirect_to '/profile'
+    new_user = User.new(user_params)
+    if new_user.save
+      session[:user_id] = new_user.id
+      redirect_to '/profile'
+      flash[:sucess] = "You have registered successfully! You are now logged in as #{new_user.name}."
+    else
+      flash[:error] = new_user.errors.full_messages.to_sentence
+      redirect_to '/register'
+    end
   end
 
   private
 
   def user_params
-    params.permit(:name, :city, :address, :city, :state, :zip, :email, :password)
+    params.permit(:name, :city, :address, :city, :state, :zip, :email, :password, :password_confirmation)
   end
 
 end
