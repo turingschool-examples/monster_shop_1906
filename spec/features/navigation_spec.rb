f3e4b94bafb0bbb18cee8a96b15aae37adffe39f
Mary Lang <mcl86@cornell.edu>
2019-10-21 21:32:52 -0600

    Add role tests to user spec

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -73,7 +73,7 @@ RSpec.describe 'Site Navigation' do
     end
   end
   describe 'As a user' do
-    it 'I see the navbar with links to all pages' do
+    it 'I see the navbar with links with profile and log out, not login or register' do
       user = User.create(name: 'Patti', address: '953 Sunshine Ave', city: 'Honolulu', state: 'Hawaii', zip: '96701', e_mail: 'pattimonkey34@gmail.com', password: 'banana')
 
       visit '/'
@@ -96,6 +96,30 @@ RSpec.describe 'Site Navigation' do
       end
     end
   end
+  describe 'As a merchant' do
+    it 'I see navbar with links to all pages, profile, logout, dashboard, not login or register' do
+      user = User.create(name: 'Ross', address: '56 HairGel Ave', city: 'Las Vegas', state: 'Nevada', zip: '65041', e_mail: 'dinosaurs_rule@gmail.com', password: 'rachel', role: 2)
 
+      visit '/'
+
+      click_link 'Login'
+
+      fill_in :e_mail, with: user.e_mail
+      fill_in :password, with: user.password
+      click_button 'Log In'
+
+      within 'nav' do
+        expect(page).to have_content('All Merchants')
+        expect(page).to have_content('All Items')
+        expect(page).to have_content('Cart: 0')
+        expect(page).to have_content('Logged in as Ross')
+        expect(page).to have_content('Log out')
+        expect(page).to have_content('Profile')
+        expect(page).to have_content('Dashboard')
+        expect(page).to_not have_content('Login')
+        expect(page).to_not have_content('Register')
+      end
+    end
+  end
 
 end
