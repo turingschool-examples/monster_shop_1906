b78674395124bb50a78c5585beb7d911ad6c18c4
Graham Thompson <grwthomps@gmail.com>
2019-10-22 16:56:27 -0600

    Modify redirect depending on user in sessions controller
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,9 @@ class SessionsController < ApplicationController
     if user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = ["#{user.name}, you have successfully logged in."]
-      redirect_to '/profile'
+      redirect_to '/profile' if user.default?
+      redirect_to '/merchant/dashboard' if user.merchant_employee? || user.merchant_admin?
+      redirect_to '/admin/dashboard' if user.admin?
     else
       flash[:error] = ["Sorry, credentials were invalid. Please try again."]
       render :new
