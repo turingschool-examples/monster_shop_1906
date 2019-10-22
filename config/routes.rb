26d746eb15606238a7aba881cb0039e55b08be3c
Jon Patterson <JonPatt92@gmail.com>
2019-10-22 11:05:03 -0600

    Add namespace routes for admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -43,4 +43,9 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/users', to: 'users#create'
   get '/profile', to: 'users#show'
+
+  namespace :admin do
+    get '/', to: 'dashboard#show'
+    get '/users', to: 'dashboard#index'
+  end
 end
