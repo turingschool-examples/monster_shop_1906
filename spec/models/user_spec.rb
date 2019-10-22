e53cc1b7c8b1755ce2657f1a80fc86a6011b10f7
Brian Bower <brian@brianbower.com>
2019-10-22 15:11:53 -0600

    Add spec for user navbar
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,19 @@ describe User, type: :model do
     it { should validate_confirmation_of :password }
   end
 
+  describe 'roles' do
+    it "can be created by a default user" do
+      user = User.create( name: 'Bob J',
+                          address: '123 Fake St',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: 80111,
+                          email: 'user@user.com',
+                          password: 'password',
+                          role: 0)
+
+      expect(user.role).to eq('default')
+      expect(user.default?).to be_truthy
+    end
+  end
 end
