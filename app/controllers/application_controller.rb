0c21cabab45259cc43fe6ec7e6e2e4e2ab17cbf4
Jon Patterson <JonPatt92@gmail.com>
2019-10-22 16:00:33 -0600

    Add current_merchant? as helper method to verify merchant user status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_admin?
+  helper_method :cart, :current_user, :current_admin?, :current_merchant?
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_merchant?
-    current_user && current_user.merchant?
+    current_user && (current_user.merchant_employee? || current_user.merchant_admin?)
   end
 end
