83f1513dae78f25773d433a671b047cbd93002f4
Graham Thompson <grwthomps@gmail.com>
2019-10-22 15:09:53 -0600

    Add roles to user model: default, merchant_employee, merchant_admin, and admin
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates_presence_of :password, require: true
   validates_presence_of :password_confirmation, require: true
   has_secure_password
+
+  enum role: %w(default merchant_employee merchant_admin admin)
 end
