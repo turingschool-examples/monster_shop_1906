779d001ae34490d40e5ad127e7b6f9389a258888
Mary Lang <mcl86@cornell.edu>
2019-10-21 18:57:04 -0600

    Add user and session controller, user model and user database

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe User do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+    it { should validate_presence_of :e_mail }
+    it { should validate_presence_of :password }
+  end
+end
