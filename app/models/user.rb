430c72791eb8b7ae3bd43ab6f26cec843366c4ec
Brian Bower <brian@brianbower.com>
2019-10-22 16:24:25 -0600

    Setup admin roles
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :email
+
   validates :email, uniqueness: true
   validates :password, presence: true, confirmation: { case_sensitive: true }
   has_secure_password
 
-  enum role: [:default, :merchant_employee, :merchant_admin]
+  enum role: [:default, :merchant_employee, :merchant_admin, :admin]
 end
