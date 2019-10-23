a0b815cf0cd8badf4130d461d1e552d9a4f8db5e
Graham Thompson <grwthomps@gmail.com>
2019-10-22 18:02:56 -0600

    Create redirect helper method, add conditional to sessions controller for already logged in users
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def new
+    if current_user
+      flash[:error] = ['Sorry, you are already logged in.']
+      redirect(current_user)
+    end
   end
 
   def create
@@ -7,12 +11,18 @@ class SessionsController < ApplicationController
     if user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = ["#{user.name}, you have successfully logged in."]
-      redirect_to '/profile' if user.default?
-      redirect_to '/merchant/dashboard' if user.merchant_employee? || user.merchant_admin?
-      redirect_to '/admin/dashboard' if user.admin?
+      redirect(user)
     else
       flash[:error] = ["Sorry, credentials were invalid. Please try again."]
       render :new
     end
+  end
+
+  private
+
+  def redirect(user)
+    redirect_to '/profile' if user.default?
+    redirect_to '/merchant/dashboard' if user.merchant_employee? || user.merchant_admin?
+    redirect_to '/admin/dashboard' if user.admin?
   end
 end
