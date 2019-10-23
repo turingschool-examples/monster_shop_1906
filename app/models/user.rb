9c2ce043161c0077c8c47b092f0cacf25c628d89
Jon Patterson <JonPatt92@gmail.com>
2019-10-23 15:37:21 -0600

    Eliminate deadspace

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   validates :email, uniqueness: true, presence: true
 
-  validates :password, :presence =>true, allow_nil: false
+  validates :password, :presence => true, allow_nil: false
 
   has_secure_password
 
   enum role: %w(default merchant_employee merchant_admin admin)
-
 end
