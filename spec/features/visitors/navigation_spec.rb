60f469d1c52874b12314410c3eb504aa9a92dd41
Zac Isaacson <isaacson.zm@gmail.com>
2019-10-23 17:35:29 -0600

    Add admin restrictions test
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- spec/features/visitors/navigation_spec.rb
+++ spec/features/visitors/navigation_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe "As a visitor" do
     visit '/merchant'
 
     expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+    visit '/admin'
+
+    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+    visit '/profile'
+
+    expect(page).to have_content("The page you were looking for doesn't exist (404)")
   end
 end
