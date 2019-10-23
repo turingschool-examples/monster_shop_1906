c043c9fd2c9ac1cda93338efd360adaf627f1ef8
John Travers <johnk.travers@gmail.com>
2019-10-23 11:30:19 -0600

    Refactor tests for visitor navigation links
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>
    Co-authored-by: Graham Thompson <51726839+grwthomps@users.noreply.github.com>

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -6,32 +6,30 @@ RSpec.describe 'Site Navigation' do
     it "I see a nav bar with links to all pages" do
       visit '/merchants'
 
-      within 'nav' do
-        click_link 'Items'
-      end
+      within('nav') { click_link 'Home' }
+      expect(current_path).to eq('/')
 
+      within('nav') { click_link 'Items' }
       expect(current_path).to eq('/items')
 
-      within 'nav' do
-        click_link 'Merchants'
-      end
-
+      within('nav') { click_link 'Merchants' }
       expect(current_path).to eq('/merchants')
+
+      within('nav') { click_link 'Login' }
+      expect(current_path).to eq('/login')
+
+      within('nav') { click_link 'Register' }
+      expect(current_path).to eq('/register')
     end
 
     it "I can see a cart indicator on all pages" do
       visit '/merchants'
 
-      within 'nav' do
-        expect(page).to have_content("Cart (0)")
-      end
+      within('nav') { expect(page).to have_content("Cart (0)") }
 
       visit '/items'
 
-      within 'nav' do
-        expect(page).to have_content("Cart (0)")
-      end
-
+      within('nav') { expect(page).to have_content("Cart (0)") }
     end
   end
 end
