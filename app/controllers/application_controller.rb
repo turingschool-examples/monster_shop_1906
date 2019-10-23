b5e150335514e25733ad85a8e6d2c3a9f7e50d6c
Jon Patterson <JonPatt92@gmail.com>
2019-10-22 18:53:33 -0600

    Add default_user? helper method to verify if user is default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_admin?, :current_merchant?
+  helper_method :cart, :current_user, :default_user?, :current_admin?, :current_merchant?
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def default_user?
+    current_user && current_user.default?
   end
 
   def current_admin?
