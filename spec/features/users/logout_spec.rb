a4531f311fd685471f24c8c509218bbe48e63a9f
Alice Post <alicep2322@gmail.com>
2019-10-22 19:14:59 -0600

    Add merchant and admin logout tests
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- spec/features/users/logout_spec.rb
+++ spec/features/users/logout_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'As a registered user, merchant, or admin' do
   describe 'When i click logout' do
-    it 'logs me out and removes the items in my cart' do
+    it 'as a user it logs me out and removes the items in my cart' do
       mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
@@ -42,6 +42,81 @@ describe 'As a registered user, merchant, or admin' do
         expect(page).to have_link('Login')
         expect(page).to have_link('Register')
         expect(page).to have_link('Cart: 0')
+        expect(page).to_not have_link('Profile')
+        expect(page).to_not have_link('Logout')
+      end
+      expect(page).to have_content("You have logged out.")
+    end
+
+    it 'as a merchant it logs me out and removes the items in my cart' do
+      mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+      pencil = mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
+      merchant = User.create( name: 'Bob J',
+                          address: '123 Fake St',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: 80111,
+                          email: 'user@user.com',
+                          password: 'password',
+                          role: 2)
+
+      visit '/login'
+
+      fill_in :email, with: 'user@user.com'
+      fill_in :password, with: 'password'
+      click_button 'Login'
+
+      visit "/items/#{paper.id}"
+      click_on "Add To Cart"
+      visit "/items/#{tire.id}"
+      click_on "Add To Cart"
+      visit "/items/#{pencil.id}"
+      click_on "Add To Cart"
+
+      within '.topnav' do
+        expect(page).to have_link('Cart: 3')
+      end
+
+      click_link 'Logout'
+
+      expect(current_path).to eq('/')
+      within '.topnav' do
+        expect(page).to have_link('Login')
+        expect(page).to have_link('Register')
+        expect(page).to have_link('Cart: 0')
+        expect(page).to_not have_link('Profile')
+        expect(page).to_not have_link('Logout')
+      end
+      expect(page).to have_content("You have logged out.")
+    end
+
+    it 'as an admin it logs me out' do
+      admin = User.create( name: 'Bob J',
+                          address: '123 Fake St',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: 80111,
+                          email: 'user@user.com',
+                          password: 'password',
+                          role: 3)
+
+      visit '/login'
+
+      fill_in :email, with: 'user@user.com'
+      fill_in :password, with: 'password'
+      click_button 'Login'
+
+      click_link 'Logout'
+
+      expect(current_path).to eq('/')
+      within '.topnav' do
+        expect(page).to have_link('Login')
+        expect(page).to have_link('Register')
         expect(page).to_not have_link('Profile')
         expect(page).to_not have_link('Logout')
       end
