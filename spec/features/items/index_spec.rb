189c039f0215d0caa1a6126c48bf4b0dbc2282fe
David Gomez <davdg25@gmail.com>
2019-10-23 11:59:50 -0600

    Add new tests to only show active items for visitors and users
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -10,6 +10,32 @@ RSpec.describe "Items Index Page" do
 
       @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
       @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+
+      @user = User.create( name: 'Bob J',
+                          address: '123 Fake St',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: 80111,
+                          email: 'user@user.com',
+                          password: 'password')
+
+      @merchant = User.create( name: 'Bob J',
+                          address: '123 Fake St',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: 80111,
+                          email: 'user@user.com',
+                          password: 'password',
+                          role: 2)
+
+      @admin = User.create( name: 'Bob J',
+                          address: '123 Fake St',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: 80111,
+                          email: 'user@user.com',
+                          password: 'password',
+                          role: 3)
     end
 
     it "all items or merchant names are links" do
@@ -19,8 +45,8 @@ RSpec.describe "Items Index Page" do
       expect(page).to have_link(@tire.merchant.name)
       expect(page).to have_link(@pull_toy.name)
       expect(page).to have_link(@pull_toy.merchant.name)
-      expect(page).to have_link(@dog_bone.name)
-      expect(page).to have_link(@dog_bone.merchant.name)
+      # expect(page).to have_link(@dog_bone.name)
+      # expect(page).to have_link(@dog_bone.merchant.name)
     end
 
     it "I can see a list of all of the items "do
@@ -47,15 +73,43 @@ RSpec.describe "Items Index Page" do
         expect(page).to have_css("img[src*='#{@pull_toy.image}']")
       end
 
-      within "#item-#{@dog_bone.id}" do
-        expect(page).to have_link(@dog_bone.name)
-        expect(page).to have_content(@dog_bone.description)
-        expect(page).to have_content("Price: $#{@dog_bone.price}")
-        expect(page).to have_content("Inactive")
-        expect(page).to have_content("Inventory: #{@dog_bone.inventory}")
-        expect(page).to have_link(@brian.name)
-        expect(page).to have_css("img[src*='#{@dog_bone.image}']")
-      end
+      # within "#item-#{@dog_bone.id}" do
+      #   expect(page).to have_link(@dog_bone.name)
+      #   expect(page).to have_content(@dog_bone.description)
+      #   expect(page).to have_content("Price: $#{@dog_bone.price}")
+      #   expect(page).to have_content("Inactive")
+      #   expect(page).to have_content("Inventory: #{@dog_bone.inventory}")
+      #   expect(page).to have_link(@brian.name)
+      #   expect(page).to have_css("img[src*='#{@dog_bone.image}']")
+      # end
     end
+
+    it 'can show undisabled items to all users and images are now links' do
+
+      visit '/login'
+
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+
+      click_button 'Login'
+
+      visit '/items'
+
+      expect(page).to have_content(@tire.name)
+      expect(page).to have_content(@pull_toy.name)
+      expect(page).to_not have_content(@dog_bone.name)
+
+      find(".image-link-#{@tire.id}").click
+
+
+      expect(current_path).to eq("/items/#{@tire.id}")
+    end
+# User Story 17, Items Index Page
+#
+# As a regular user on the system
+# I can visit the items catalog ("/items")
+# I see all items in the system except disabled items
+#
+# The item image is a link to that item's show page
   end
 end
