fd153fb251b8ec71ef64a85d766fcc865ec060a5
Laura Schulz <lrs8810@gmail.com>
2019-10-23 11:59:08 -0600

    Add admin nav spec for admin dashboard and admin users
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>
    Co-Authored-By: Michael Cooper <CoopTang@users.noreply.github.com>

---
+++ spec/features/users/admin_nav_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'As an admin' do
+  it 'has a link to the admin dashboard' do
+    admin = User.create(username:'funbucket14', password: 'secure', role: 3)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit '/'
+
+    within 'nav' do
+      click_link 'Admin Dashboard'
+    end
+
+    expect(current_path).to eq('/admin')
+
+    within 'nav' do
+      click_link 'Users'
+    end
+
+    expect(current_path).to eq('/admin/users')
+
+    within 'nav' do
+      expect(page).to_not have_link('Cart')
+    end
+
+  end
+end
