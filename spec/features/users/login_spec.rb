f46afd1af13095ce8d92ce7e825e1fef4bf8866b
Graham Thompson <grwthomps@gmail.com>
2019-10-22 18:00:12 -0600

    Add tests for redirecting users if already logged in and they go to login path
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -36,24 +36,41 @@ RSpec.describe 'User login page' do
       expect(current_path).to eq('/login')
       expect(page).to have_content("Sorry, credentials were invalid. Please try again.")
     end
+
+    it 'redirects to user profile from login path if user is logged in' do
+      visit '/login'
+
+      fill_in :email, with: 'test@gmail.com'
+      fill_in :password, with: 'password123'
+
+      click_button 'Login'
+
+      visit '/login'
+
+      expect(current_path).to eq('/profile')
+
+      expect(page).to have_content('Sorry, you are already logged in.')
+    end
   end
 
   describe 'merchant user' do
+    before :each do
+      @employee = User.create!(name: "Gmoney", address: "123 Lincoln St", city: "Denver", state: "CO", zip: 23840, email: "test1@gmail.com", password: "password123", password_confirmation: "password123", role: 1)
+      @admin = User.create!(name: "Gmoney", address: "123 Lincoln St", city: "Denver", state: "CO", zip: 23840, email: "test@gmail.com", password: "password123", password_confirmation: "password123", role: 2)
+    end
     it 'employee needs to login with valid credentials' do
-      employee = User.create!(name: "Gmoney", address: "123 Lincoln St", city: "Denver", state: "CO", zip: 23840, email: "test@gmail.com", password: "password123", password_confirmation: "password123", role: 1)
       visit '/login'
 
-      fill_in :email, with: 'test@gmail.com'
+      fill_in :email, with: 'test1@gmail.com'
       fill_in :password, with: 'password123'
 
       click_button 'Login'
 
       expect(current_path).to eq('/merchant/dashboard')
-      expect(page).to have_content("#{employee.name}, you have successfully logged in.")
+      expect(page).to have_content("#{@employee.name}, you have successfully logged in.")
     end
 
     it 'admin needs to login with valid credentials' do
-      admin = User.create!(name: "Gmoney", address: "123 Lincoln St", city: "Denver", state: "CO", zip: 23840, email: "test@gmail.com", password: "password123", password_confirmation: "password123", role: 2)
       visit '/login'
 
       fill_in :email, with: 'test@gmail.com'
@@ -62,13 +79,45 @@ RSpec.describe 'User login page' do
       click_button 'Login'
 
       expect(current_path).to eq('/merchant/dashboard')
-      expect(page).to have_content("#{admin.name}, you have successfully logged in.")
+      expect(page).to have_content("#{@admin.name}, you have successfully logged in.")
+    end
+
+    it 'redirects to merchant dashboard from login path if employee is logged in' do
+      visit '/login'
+
+      fill_in :email, with: 'test1@gmail.com'
+      fill_in :password, with: 'password123'
+
+      click_button 'Login'
+
+      visit '/login'
+
+      expect(current_path).to eq('/merchant/dashboard')
+
+      expect(page).to have_content('Sorry, you are already logged in.')
+    end
+
+    it 'redirects to merchant dashboard from login path if admin is logged in' do
+      visit '/login'
+
+      fill_in :email, with: 'test@gmail.com'
+      fill_in :password, with: 'password123'
+
+      click_button 'Login'
+
+      visit '/login'
+
+      expect(current_path).to eq('/merchant/dashboard')
+
+      expect(page).to have_content('Sorry, you are already logged in.')
     end
   end
 
   describe 'admin' do
+    before :each do
+      @admin = User.create!(name: "Gmoney", address: "123 Lincoln St", city: "Denver", state: "CO", zip: 23840, email: "test@gmail.com", password: "password123", password_confirmation: "password123", role: 3)
+    end
     it 'needs to login with valid credentials' do
-      admin = User.create!(name: "Gmoney", address: "123 Lincoln St", city: "Denver", state: "CO", zip: 23840, email: "test@gmail.com", password: "password123", password_confirmation: "password123", role: 3)
       visit '/login'
 
       fill_in :email, with: 'test@gmail.com'
@@ -77,7 +126,22 @@ RSpec.describe 'User login page' do
       click_button 'Login'
 
       expect(current_path).to eq('/admin/dashboard')
-      expect(page).to have_content("#{admin.name}, you have successfully logged in.")
+      expect(page).to have_content("#{@admin.name}, you have successfully logged in.")
+    end
+
+    it 'redirects to admin dashboard from login path if admin is logged in' do
+      visit '/login'
+
+      fill_in :email, with: 'test@gmail.com'
+      fill_in :password, with: 'password123'
+
+      click_button 'Login'
+
+      visit '/login'
+
+      expect(current_path).to eq('/admin/dashboard')
+
+      expect(page).to have_content('Sorry, you are already logged in.')
     end
   end
 end
