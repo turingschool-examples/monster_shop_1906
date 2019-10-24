62f41b7bd088856cba2392db4504befd65818cb1
Zac Isaacson <isaacson.zm@gmail.com>
2019-10-24 10:54:21 -0600

    Add seed data for all user types
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 Merchant.destroy_all
 Item.destroy_all
+User.destroy_all
 
 #merchants
 bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -19,3 +20,8 @@ tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never po
 #dog_shop items
 pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+
+default_user = User.create(name: "Lynda Ferguson", address: "452 Cherry St", city: "Tucson", state: "AZ", zip: 85736, email: "lferguson@gmail.com", password: "password1", password_confirmation: "password1")
+merchant_employee = User.create(name: "Lael Whipple", address: "7392 Oklahoma Ave", city: "Nashville", state: "TN", zip: 37966, email: "whip_whipple@yahoo.com", password: "password12", password_confirmation: "password12", role: 1)
+merchant_admin = User.create(name: "Dudley Laughlin", address: "2348 Willow Dr", city: "Big Sky", state: "MT", zip: 59716, email: "bigskyguy@aol.com", password: "password123", password_confirmation: "password123", role: 2)
+admin = User.create(name: "Dorian Bouchard", address: "7890 Montreal Blvd", city: "New Orleans", state: "LA", zip: 70032, email: "ouibouchard@gmail.fr", password: "password1234", password_confirmation: "password1234", role:3)
