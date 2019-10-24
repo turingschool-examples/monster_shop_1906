bfc7f7e6922ecf4c3da0cda14b4b49da5c1eac88
John Travers <johnk.travers@gmail.com>
2019-10-24 14:15:59 -0600

    Add class methods for top and bottom five ordered items
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>
    Co-authored-by: Graham Thompson <51726839+grwthomps@users.noreply.github.com>

--- app/models/item.rb
+++ app/models/item.rb
@@ -34,6 +34,10 @@ class Item <ApplicationRecord
   end
 
   def self.top_five_ordered
-    binding.pry
+    joins(:item_orders).group('items.id').order('sum(item_orders.quantity) DESC').limit(5)
+  end
+
+  def self.bottom_five_ordered
+    joins(:item_orders).group('items.id').order('sum(item_orders.quantity)').limit(5)
   end
 end
