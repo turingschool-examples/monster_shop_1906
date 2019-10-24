12e3b18a4cce4ac0275e8d79d488a239c4228fb6
Zac Isaacson <isaacson.zm@gmail.com>
2019-10-24 10:33:07 -0600

    Add navigation restriction test for merchant admins
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

---
+++ spec/features/merchant_admins/navigation_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "As a merchant admin" do
+  it "cannot access certain paths" do
+    merchant_admin = User.create!(name: "Gmoney", address: "123 Lincoln St", city: "Denver", state: "CO", zip: 23840, email: "test@gmail.com", password: "password123", password_confirmation: "password123", role: 2)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_admin)
+
+    visit '/merchants'
+
+    expect(page).to have_content("Logged in as Gmoney")
+
+    visit '/admin'
+
+    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+  end
+end
