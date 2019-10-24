5105a9d74993b1166b68df9198c7e298625738c3
David Gomez <davdg25@gmail.com>
2019-10-24 17:51:53 -0600

    Refactor create method and update order params
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- app/controllers/profile/orders_controller.rb
+++ app/controllers/profile/orders_controller.rb
@@ -3,25 +3,20 @@ class Profile::OrdersController < ApplicationController
   def index
 
   end
-  
+
   def create
     if current_user
       order = Order.create(order_params)
-      if order.save
-        cart.items.each do |item,quantity|
-          order.item_orders.create({
-            item: item,
-            quantity: quantity,
-            price: item.price
-            })
-        end
-        session.delete(:cart)
-        flash[:success] = 'Thank you for placing your order'
-        redirect_to "/profile/orders"
-      else
-        flash[:notice] = "Please complete address form to create an order."
-        render :new
+      cart.items.each do |item,quantity|
+        order.item_orders.create({
+          item: item,
+          quantity: quantity,
+          price: item.price
+          })
       end
+      session.delete(:cart)
+      flash[:success] = 'Thank you for placing your order'
+      redirect_to "/profile/orders"
     else
       flash[:error] = 'Please login or register to continue'
       redirect_to '/cart'
@@ -32,6 +27,6 @@ class Profile::OrdersController < ApplicationController
   private
 
   def order_params
-    {name: current_user.name, address: current_user.address, city: current_user.city, state: current_user.state, zip: current_user.zip}
+    {user_id: current_user.id, name: current_user.name, address: current_user.address, city: current_user.city, state: current_user.state, zip: current_user.zip}
   end
 end
