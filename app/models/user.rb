0add62d0731fb5a88991d42fe36f20e86063f494
John Travers <johnk.travers@gmail.com>
2019-10-25 15:40:56 -0600

    Add if statements to password and password confirmation requirements
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>
    Co-authored-by: Graham Thompson <51726839+grwthomps@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,9 @@
 class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password, require: true
-  validates_presence_of :password_confirmation, require: true
+  validates_presence_of :password, require: true, if: :password
+  validates_presence_of :password_confirmation, require: true, if: :password_confirmation
   has_secure_password
 
   enum role: %w(default merchant_employee merchant_admin admin)
 end
-
-
-# validates :password,
-#           presence: true,
-#           length: { minimum: 2 },
-#           confirmation: { case_sensitive: true },
-#           if: :password
-#
-# validates :password_confirmation,
-#           presence: true,
-#           if: :password
