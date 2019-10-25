663131ba853220f1fab61328af2ee2caf2d13b76
Alice Post <alicep2322@gmail.com>
2019-10-25 16:12:23 -0600

    Order instance methods to format dates and return the items count in an order
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,4 +8,16 @@ class Order <ApplicationRecord
   def grandtotal
     item_orders.sum('price * quantity')
   end
+
+  def created_date
+    created_at.strftime('%B %d, %Y')
+  end
+
+  def updated_date
+    updated_at.strftime('%B %d, %Y')
+  end
+
+  def item_count
+    item_orders.sum(:quantity)
+  end
 end
