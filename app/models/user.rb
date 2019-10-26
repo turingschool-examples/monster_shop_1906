ccaf3777c1307ef1f5c0779797ee11c95db0048a
John Travers <johnk.travers@gmail.com>
2019-10-25 18:33:42 -0600

    Remove unnecessary password and password confirmation validations
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>
    Co-authored-by: Graham Thompson <51726839+grwthomps@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password, require: true, if: :password
-  validates_presence_of :password_confirmation, require: true, if: :password_confirmation
   has_secure_password
 
   enum role: %w(default merchant_employee merchant_admin admin)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :zip}
     it { should validate_presence_of :email}
     it { should validate_uniqueness_of :email}
-    it { should validate_presence_of :password}
-    it { should validate_presence_of :password_confirmation}
   end
 
   describe 'roles' do
