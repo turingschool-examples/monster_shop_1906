44d125dd30fb3f1a9a6841475005b982956db861
Michael Cooper <macoop2363@gmail.com>
2019-10-25 21:22:31 -0600

    Add sad path functionality so users can't access orders that don't exist or orders that aren't theirs

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -3,7 +3,8 @@ class UserOrdersController < ApplicationController
   def index; end
 
   def show
-    @order = Order.find(params[:id])
+    @order = Order.where(id: params[:id], user_id: current_user.id).first
+    render_404 unless @order
   end
 
 end
