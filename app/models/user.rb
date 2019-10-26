41dda4b1aefa940702eddc6dba5014ad04155b0b
Graham Thompson <grwthomps@gmail.com>
2019-10-26 16:28:07 -0600

    Add one-to-many relationship in user and order models
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   validates :email, uniqueness: true, presence: true
   has_secure_password
+  has_many :orders
 
   enum role: %w(default merchant_employee merchant_admin admin)
 end
