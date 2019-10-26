e9eecd4e935f383f964de6d6ccd17b5ffff06a9f
Graham Thompson <grwthomps@gmail.com>
2019-10-26 16:36:13 -0600

    Edit orders controller to redirect to '/profile/orders' instead of "/orders/#{order.id}"
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController <ApplicationController
   end
 
   def create
-    order = Order.create(order_params)
+    order = Order.create((order_params).merge(user_id: current_user.id))
     if order.save
       cart.items.each do |item,quantity|
         order.item_orders.create({
@@ -19,13 +19,14 @@ class OrdersController <ApplicationController
           })
       end
       session.delete(:cart)
-      redirect_to "/orders/#{order.id}"
+      # redirect_to "/orders/#{order.id}"
+      flash[:success] = ['Your order has been successfully created!']
+      redirect_to '/profile/orders'
     else
       flash[:notice] = ["Please complete address form to create an order."]
       render :new
     end
   end
-
 
   private
 
