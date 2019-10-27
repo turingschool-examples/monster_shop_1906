032e6ea44fc57fc348dc910b0d38fe89a73a907f
Graham Thompson <grwthomps@gmail.com>
2019-10-27 13:16:40 -0600

    Create new migration to remove columns from orders table
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191026194210) do
+ActiveRecord::Schema.define(version: 20191027171716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,11 +50,6 @@ ActiveRecord::Schema.define(version: 20191026194210) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.integer "zip"
     t.string "status", default: "Pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
