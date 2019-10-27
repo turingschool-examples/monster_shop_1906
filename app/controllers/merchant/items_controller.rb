2b68ddc2e961f01bbc9957a561692bfa246f7fe3
Brian Bower <brian@brianbower.com>
2019-10-27 16:21:19 -0600

    Add filfill action
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -3,4 +3,20 @@ class Merchant::ItemsController < Merchant::BaseController
   def index
 
   end
+
+  def fulfill
+    item = Item.find(params[:item_id])
+    item_order = item.item_orders.find_by(order_id: params[:order_id])
+    if item_order.status != 'fulfilled'
+      item_order.update(status: 'fulfilled')
+      new_inventory = item.inventory - item_order.quantity
+      item.update(inventory: new_inventory)
+      flash[:success] = "You have fulfilled #{item.name}."
+      redirect_to "/merchant/orders/#{params[:order_id]}"
+    else
+      flash[:error] = "You have already fulfilled this item."
+      redirect_to "/merchant/orders/#{params[:order_id]}"
+    end
+  end
+
 end
