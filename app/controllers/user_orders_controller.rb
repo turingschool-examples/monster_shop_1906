1648dc9f126d04c42c2f165facb721110f3b8fd3
Zac Isaacson <isaacson.zm@gmail.com>
2019-10-27 17:10:04 -0600

    Add show and update functionality to user orders controller
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -1,5 +1,28 @@
 class UserOrdersController < ApplicationController
+  before_action :require_user
+
   def index
     @orders = current_user.orders
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
+  end
+
+  def update
+    order = Order.find(params[:id])
+    order.update(status: 'cancelled')
+    order.item_orders.each do |item_order|
+      item_order.update(status: 'cancelled')
+    end
+    flash[:success] = ['Your order is now cancelled']
+    redirect_to '/profile'
+  end
+
+
+  private
+
+  def require_user
+    render file: "/public/404" unless current_user
   end
 end
