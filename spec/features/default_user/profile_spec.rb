a6b58b4166a19ceb2dfeb2e345838b2803cfbeaa
Zac Isaacson <isaacson.zm@gmail.com>
2019-10-27 17:02:48 -0600

    Add test for link to user orders
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- spec/features/default_user/profile_spec.rb
+++ spec/features/default_user/profile_spec.rb
@@ -97,4 +97,24 @@ RSpec.describe 'As a default user' do
     expect(page).to have_field :password
     expect(page).to have_field :password_confirmation
   end
+
+  it "shows a link to orders if user has orders" do
+    meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+    order_1 = Order.create!(user_id: @user.id)
+    order_1.item_orders.create!(item_id: tire.id, price: tire.price, quantity: 2)
+
+    visit '/profile'
+
+    click_link 'Your Orders'
+
+    expect(current_path).to eq('/profile/orders')
+  end
+
+  it "does't have link to orders if user doesn't have any orders" do
+    visit '/profile'
+
+    expect(page).to_not have_link('Your Orders')
+  end
 end
