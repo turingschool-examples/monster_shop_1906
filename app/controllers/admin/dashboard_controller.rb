6d957849d5a08d9745b382535c4fd14c6c11b93d
John Travers <johnk.travers@gmail.com>
2019-10-28 14:35:22 -0600

    Add merchant_index action to admin dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class Admin::DashboardController < Admin::BaseController
   def index
   end
+
+  def merchant_index
+    merchants = Merchant.where(id: params[:id])
+    if merchants.empty?
+      render_404
+    else
+      @merchant = merchants.first
+    end
+  end
 end
