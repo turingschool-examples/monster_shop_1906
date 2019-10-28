2674eef257eb51c2e2d09445aa3a3dec045deffc
John Travers <johnk.travers@gmail.com>
2019-10-28 01:41:44 -0600

    Fix error so that merchant users can view order show pages that include their items
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>
    Co-authored-by: Graham Thompson <51726839+grwthomps@users.noreply.github.com>

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -6,7 +6,7 @@ class UserOrdersController < ApplicationController
   end
 
   def show
-    @order = current_user.orders.find(params[:id])
+    @order = Order.find(params[:id])
   end
 
   def update
