cddfb25fb9c5ff14d136cc66fbd781a4b134f100
John Travers <johnk.travers@gmail.com>
2019-10-28 20:52:33 -0600

    Add update action to change item order status to fulfilled and decrement item inventory

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -8,4 +8,16 @@ class Merchant::OrdersController < Merchant::BaseController
       @order = orders.first
     end
   end
+
+  def update
+    item_order = ItemOrder.find(params[:item_order_id])
+    item_order.update(status: 'fulfilled')
+
+    item = Item.find(item_order.item_id)
+    item.reduce_quantity(item_order.quantity)
+    item.save
+
+    flash[:success] = ["You have successfully fulfilled #{item.name} for Order ##{item_order.order_id}"]
+    redirect_to "/merchant/orders/#{item_order.order_id}"
+  end
 end
