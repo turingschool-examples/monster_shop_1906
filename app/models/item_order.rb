face45acd8125cea8980350dfcbe991599e37597
John Travers <johnk.travers@gmail.com>
2019-10-28 20:47:48 -0600

    Add instance methods for boolean logic used in merchant order show page

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -7,4 +7,20 @@ class ItemOrder <ApplicationRecord
   def subtotal
     price * quantity
   end
+
+  def unfulfilled?
+    status == 'unfulfilled'
+  end
+
+  def fulfilled?
+    status == 'fulfilled'
+  end
+
+  def cancelled?
+    status == 'cancelled'
+  end
+
+  def enough_inventory?
+    quantity <= item.inventory
+  end
 end
