8e0500b1aea5ab235e223297218cffe3c9ab0104
Graham Thompson <grwthomps@gmail.com>
2019-10-29 13:14:13 -0600

    Refactor order status enum to be lowercase
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order <ApplicationRecord
   has_many :items, through: :item_orders
   belongs_to :user
 
-  enum status: %w(Packaged Pending Shipped Cancelled)
+  enum status: %w(packaged pending shipped cancelled)
 
   def grandtotal
     item_orders.sum('price * quantity')
