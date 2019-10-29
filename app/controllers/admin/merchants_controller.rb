9ed87142ec76672092be66dce7a14002e71c31be
Zac Isaacson <isaacson.zm@gmail.com>
2019-10-29 08:22:14 -0600

    Refactor merchant update controller
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,18 +2,12 @@ class Admin::MerchantsController < Admin::BaseController
   def update
     merchant = Merchant.find(params[:id])
     merchant.toggle! :enabled?
-    # merchant.items.each do |item|
-    #   item.toggle! :active?
-    # end
+    merchant.items.each do |item|
+      item.update(active?: merchant.enabled?)
+    end
     if merchant.enabled?
-      merchant.items.each do |item|
-        item.update(active?: true)
-      end
         flash[:success] = ["#{merchant.name} has been enabled"]
     else
-      merchant.items.each do |item|
-        item.update(active?: false)
-      end
       flash[:success] = ["#{merchant.name} has been disabled"]
     end
     redirect_to '/merchants'
