cd6aacc417ae949c5c73f459bc7f54e8aa497153
John Travers <johnk.travers@gmail.com>
2019-10-28 20:51:02 -0600

    Add tests that merchant users can fulfill orders if enough inventory

--- spec/features/merchant_admins/order_fulfillment_spec.rb
+++ spec/features/merchant_admins/order_fulfillment_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'When I visit an order show page as a merchant employee' do
     @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd', city: 'Denver', state: 'CO', zip: 80203)
     @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd', city: 'Denver', state: 'CO', zip: 80203)
     @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-    @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 4)
+    @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
     @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
 
     @order = Order.create!(user_id: @user.id)
@@ -15,7 +15,7 @@ RSpec.describe 'When I visit an order show page as a merchant employee' do
     @order.item_orders.create!(item_id: @pencil.id, price: @pencil.price, quantity: 104)
     @order.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 3)
 
-    @employee = @mike.users.create!(name: "Harry", address: "123 Cherry St", city: "Augusta", state: "ME", zip: 23840, email: "test1@gmail.com", password: "password123", password_confirmation: "password123", role: 1)
+    @admin = @mike.users.create!(name: "Harry", address: "123 Cherry St", city: "Augusta", state: "ME", zip: 23840, email: "test1@gmail.com", password: "password123", password_confirmation: "password123", role: 2)
 
     visit '/login'
 
@@ -68,7 +68,7 @@ RSpec.describe 'When I visit an order show page as a merchant employee' do
 
     visit "/items/#{@paper.id}"
 
-    expect(page).to have_content("Inventory: 1")
+    expect(page).to have_content("Inventory: 0")
   end
 
   it 'cannot fulfill an item if it does not have enough inventory' do
