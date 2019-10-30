3e5bd48fcf4affd2489339458a3bf64790c42249
John Travers <johnk.travers@gmail.com>
2019-10-29 18:51:15 -0600

    Add sad path defense to show action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,10 +4,11 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def show
-    @user = User.find(params[:id])
-  end
-
-  def show
-    @user = User.find(params[:id])
+    users = User.where(id: params[:id])
+    if users.empty?
+      render_404
+    else
+      @user = users.first
+    end
   end
 end
