0fc8dbf72bd9a6e9ac80da21575de9ef5e1e6bda
John Travers <johnk.travers@gmail.com>
2019-10-29 20:10:10 -0600

    Add show action with sad path conditions to user orders controller

--- app/controllers/admin/user_orders_controller.rb
+++ app/controllers/admin/user_orders_controller.rb
@@ -7,4 +7,18 @@ class Admin::UserOrdersController < Admin::BaseController
       @orders = users.first.orders
     end
   end
+
+  def show
+    users = User.where(id: params[:user_id])
+    if users.empty?
+      render_404
+    else
+      orders = users.first.orders.where(id: params[:order_id])
+      if orders.empty?
+        render_404
+      else
+        @order = orders.first
+      end
+    end
+  end
 end
