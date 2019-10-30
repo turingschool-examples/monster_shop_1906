40b5d90f1f06e7b5324244594af183fce295eb0f
John Travers <johnk.travers@gmail.com>
2019-10-30 17:30:07 -0600

    Fix bug where user can add items to cart over item quantity, add flash messages
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,7 +4,7 @@ class CartController < ApplicationController
   def add_item
     item = Item.find(params[:item_id])
     cart.add_item(item.id.to_s)
-    flash[:success] = ["#{item.name} was successfully added to your cart"]
+    flash[:success] = ["#{item.name} was successfully added to your cart"] unless cart.limit_reached?(params[:item_id])
     redirect_to "/items"
   end
 
@@ -19,12 +19,15 @@ class CartController < ApplicationController
 
   def remove_item
     session[:cart].delete(params[:item_id])
+    item = Item.find(params[:item_id])
+    flash[:error] = ["#{item.name} has been removed from your cart"]
     redirect_to '/cart'
   end
 
   def increment_decrement
     if params[:increment_decrement] == "increment"
       cart.add_quantity(params[:item_id]) unless cart.limit_reached?(params[:item_id])
+      flash[:error] = ['You have reached the inventory limit for this item']  if cart.limit_reached?(params[:item_id])
     elsif params[:increment_decrement] == "decrement"
       cart.subtract_quantity(params[:item_id])
       return remove_item if cart.quantity_zero?(params[:item_id])
