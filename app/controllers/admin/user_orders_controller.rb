31d08c00958b145a191ffb98fb701700dafcf5ce
John Travers <johnk.travers@gmail.com>
2019-10-29 21:13:40 -0600

    Add update action to change order status to shipped

--- app/controllers/admin/user_orders_controller.rb
+++ app/controllers/admin/user_orders_controller.rb
@@ -21,4 +21,10 @@ class Admin::UserOrdersController < Admin::BaseController
       end
     end
   end
+
+  def update
+    order = Order.find(params[:order_id])
+    order.update(status: 2)
+    redirect_to '/admin'
+  end
 end
