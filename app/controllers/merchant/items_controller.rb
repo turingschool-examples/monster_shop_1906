a1d22c957f7e3c7395871eee32465436ab5231d4
Graham Thompson <grwthomps@gmail.com>
2019-10-30 14:15:14 -0600

    Add update, edit, destroy, new, and create actions to merchant items controller
    Co-Authored-By: null <zacisaacson@users.noreply.github.com>

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,5 +1,69 @@
 class Merchant::ItemsController < Merchant::BaseController
+  before_action :require_merchant_admin, except: [:index]
+
   def index
     @merchant = current_user.merchant
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if request.query_string == 'toggle_status'
+      @item.toggle! :active?
+      if @item.active?
+        flash[:success] = ["#{@item.name} is now for sale"]
+      else
+        flash[:error] = ["#{@item.name} is no longer for sale"]
+      end
+      redirect_to '/merchant/items'
+    else
+      @item.update(item_params)
+      if @item.save
+        flash.now[:success] = ["#{@item.name} has been successfully updated"]
+        redirect_to '/merchant/items'
+      else
+        flash.now[:error] = @item.errors.full_messages
+        render :edit
+      end
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.item_orders.empty?
+      item.destroy
+      flash[:success] = ["#{item.name} has been successfully deleted"]
+    end
+    redirect_to '/merchant/items'
+  end
+
+  def new
+    @merchant = current_user.merchant
+    @item = Item.new
+  end
+
+  def create
+    @merchant = current_user.merchant
+    @item = @merchant.items.create(item_params)
+    if @item.save
+      flash[:success] = ["#{@item.name} has been successfully created"]
+      redirect_to '/merchant/items'
+    else
+      flash.now[:error] = @item.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :image, :inventory)
+  end
+
+  def require_merchant_admin
+    render_404 unless current_user.merchant_admin?
   end
 end
